aea37a617b464dbce28520bd529c2eca119f2205
priya <priyakamani08@gmail.com>
2024-08-10 18:26:12 +0530

    10-08-2024

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-
-
 // const express = require('express');
 // const app = express();
-// // const data = require('./friend.json');
+// const data = require('./friend.json');
 // const fs = require('fs');
 // const data = fs.readFilesync('./friend.json', 'utf-8');
-// // console.log(data);
+// console.log(data);
 
-// // POST, GET, PUT, PATCH, DELETE
+// const Myfun = (req, res, next) => {
+//     // console.log(req.query);
+//     if(req.query.age >= "19")
+//     {
+//         console.log('Success');
+//         next();        
+//     }
+//     else
+//     {
+//         res.json({message: "Sorry....."})
+//     }
+    
+// }
+
+// server.use(Myfun); //application
+
+// POST, GET, PUT, PATCH, DELETE
 // Server.get("/", (req,res)=>{
 //     res.write('Welcome to Express Server');
 //     res.end();
 // })
 
-// // Server.get("/", (req,res)=>{
-// //     res.write('GET Method - 1');
-// //     res.end();
-// // })
+// serever.get("/login", Myfun, (req,res)=>{
+//     res.write('Welcome to Login Page');
+//     res.end();
+// })
 
 // Server.post("/", (req,res)=>{
 //     // res.write('Welcome to Post Method');
 //     res.send('<h1>Send Method</h1>');
 // })
 
-// server.put("/", (req,res)=>{
-//     res.json({message: 'Hello'});
-// })
-
-// server.patch("/", (res,req)=>{
-//     res.status(400);
-//     res.json({message:'Hello'})
-// })
-
-// server.get("/user", (req,res)=>{
-//     res.json(json.parse(data));
-// })
-
-// serever.get("/login", (req,res)=>{
-//     res.write('Welcome to Login Page');
-//     res.end();
-// })
-
-// server.listen(2304, ()=>{
-//     console.log('Server Start at http://localhost:2304');   
+// server.listen(2304, () => {
+//     console.log('server start at http://localhost:2304');
 // })
